26a3902adc6d8050e128a82f5c297accc850cf72
amirhosein <gooyacoder@gmail.com>
2020-09-10 21:52:15 +0430

    Upto page 106

--- routes/web.php
+++ routes/web.php
@@ -3,14 +3,23 @@
 use App\Article;
 use App\Tag;
 
-Route::get('/', function () {
-	$articles = Article::all();
-	$tags = Tag::all();
-	return view('welcome', ['articles' => $articles, 'tags' => $tags]);
-});
+// Route::get('/', function () {
+// 	$articles = Article::all();
+// 	$tags = Tag::all();
+// 	return view('welcome', ['articles' => $articles, 'tags' => $tags]);
+// });
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::resource('tasks', 'TaskController');
+// Route::resource('tasks', 'TaskController');
+
+Auth::routes();
+Route::get('/home', 'HomeController@index')->name('home');
+Route::resource('users', 'UserController');
+Route::resource('profiles', 'ProfileController');
+Route::resource('articles', 'ArticleController');
+Route::resource('comments', 'CommentController');
+Route::get('/users/{id}/articles', 'ArticleController@articles');
+Route::get('/', 'ArticleController@main');
