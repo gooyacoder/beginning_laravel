eb9e593d9683e579b1baf77166aeb317d2fa27e1
amirhosein <gooyacoder@gmail.com>
2020-09-17 14:32:37 +0430

    Upto page 121

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -30,7 +30,7 @@
             @endforeach
             </div>
         </aside> -->
-        <aside class="col-md-4 blog-sidebar">
+        <!-- <aside class="col-md-4 blog-sidebar">
             <div class="p-3">
                 <h3 class="blog-post-title">This week you have showcased only
                 the articles
@@ -38,6 +38,25 @@
                 /articles">{{ $article->user->name }}</a>
                 </h3>
                 <strong>Showing the first four results</strong>
+                <hr class="linenums" color="red">
+                <h4 class="font-italic">Tags Cloud</h4>
+                @foreach($tags as $tag)
+                <font class="font-italic" color="gray"> {{ $tag->tag }}...
+                @endforeach
+            </div>
+        </aside> -->
+        <aside class="col-md-4 blog-sidebar">
+            <div class="p-3">
+                <h3 class="blog-post-title">This week we showcase the articles
+                Written by <p></p>
+                <a href="/users/{{ $article->user_id }}/articles">
+                {{ $article->user->name }}</a>
+                </h3>
+                <strong>Showing the first four results</strong><p></p>
+                <italic>His first article is:</italic>
+                <p></p>
+                <a href="/articles/{{ $article->user->article->id }}">
+                {{ $article->user->find($article->user_id)->article->title }}</a>
                 <hr class="linenums" color="red">
                 <h4 class="font-italic">Tags Cloud</h4>
                 @foreach($tags as $tag)
